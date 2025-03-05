5f5a4844715d8b55360c8b18dcc726125a8c0a8a
hannah1590 <hannah.fasco@mymail.champlain.edu>
2025-03-04 21:14:08 -0500

    limited the color pallet of the compressed image

--- Picross/sources/main.cpp
+++ Picross/sources/main.cpp
@@ -4,61 +4,60 @@
 #include "raygui.h"
 
 #include <iostream>
-#include <set>
 #include <algorithm>
 #include <map>
+#include <vector>
+#include <cmath>
 
 #define SCREEN_WIDTH (1600)
 #define SCREEN_HEIGHT (900)
 
 #define WINDOW_TITLE "Picross"
 
-class ColorComparer
+class TextureTile
 {
 public:
-    ColorComparer()
-    {
-        r = 255;
-        g = 255;
-        b = 255;
-        a = 255;
+    TextureTile() = default;
 
-        color.r = r;
-        color.g = g;
-        color.b = b;
-        color.a = a;
+    TextureTile(Image& image)
+    {
+        texture = LoadTextureFromImage(image);
+   
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int a = 0;
+
+        // Find average rgba value for the entire image
+        for (int y = 0; y < image.height; y++)
+        {
+            for (int x = 0; x < image.width; x++)
+            {
+                Color currentColor = GetImageColor(image, x, y);
+                r += currentColor.r;
+                g += currentColor.g;
+                b += currentColor.b;
+                a += currentColor.a;
+            }
+        }
+        r /= (image.width) * (image.height);
+        g /= (image.width) * (image.height);
+        b /= (image.width) * (image.height);
+        a /= (image.width) * (image.height);
+        imageColor.r = r;
+        imageColor.g = g;
+        imageColor.b = b;
+        imageColor.a = a;
+        hsv = ColorToHSV(imageColor);
+        frequency = 0;
     }
 
-    ColorComparer(int r, int g, int b, int a)
-    {
-        this->r = r;
-        this->g = g;
-        this->b = b;
-        this->a = a;
-
-        color.r = r;
-        color.g = g;
-        color.b = b;
-        color.a = a;
-    }
-
-
-    bool operator==(ColorComparer const& rhs)
-    {
-        return (r == rhs.r) && (g == rhs.g) && (b == rhs.b);
-    }
-
-    int r;
-    int g;
-    int b;
-    int a;
-    Color color;
+//private:
+    Texture2D texture;
+    Color imageColor;
+    Vector3 hsv; // x = hue, s = saturation, v = value
+    int frequency;
 };
-
-bool operator<(ColorComparer const& lhs, ColorComparer const& rhs)
-{
-    return (lhs.r < rhs.r) && (lhs.g < rhs.g) && (lhs.b < rhs.b);
-}
 
 int main(void)
 {
@@ -66,27 +65,18 @@ int main(void)
     SetTargetFPS(60);
     SetExitKey(0);
 
-    const int ROWS = 25;
-    const int COLS = 25;
-    const float PADDING = 1.5;
-    const int COLOR_AMOUNT = 25;
+    const int ROWS = 50;
+    const int COLS = 50;
+    const float PADDING = 1; // used to expand space in between tiles
+    const int COLOR_AMOUNT = 10; // max colors allowed in puzzle
     
     Image image = LoadImage(ASSETS_PATH"test.jpg");
-    Texture baseTexture = LoadTextureFromImage(image);
+    Texture2D baseTexture = LoadTextureFromImage(image);
 
-    //Image grayscale = ImageFromChannel(image, 0);
-    //Texture grayTexture = LoadTextureFromImage(grayscale);
-
-    Texture textures[ROWS * COLS];
-    ColorComparer avgColors[ROWS * COLS];
+    TextureTile* textures[ROWS * COLS];
+    std::map<float, int> grayFrequency; // first is gray value, second is pos in array for first occurance
     float width = image.width;
     float height = image.height;
-    
-    Color totalAvg;
-    totalAvg.r = 0;
-    totalAvg.g = 0;
-    totalAvg.b = 0;
-    totalAvg.a = 0;
 
     for (int i = 0; i < ROWS; i++)
     {
@@ -99,55 +89,48 @@ int main(void)
             rect.y = i * rect.height;
 
             Image cropped = ImageFromImage(image, rect);
-            Texture2D texture = LoadTextureFromImage(cropped);
+            textures[(i * COLS) + j] = new TextureTile(cropped);
 
-            Color avgColor;
-            int r = 0;
-            int g = 0;
-            int b = 0;
-            int a = 0;
-
-            for (int y = 0; y < cropped.height; y++)
+            // Calculate highest frequency value occuring throughout the image 
+            float value = textures[(i * COLS) + j]->hsv.z;
+            if (value < 0.95 && grayFrequency.find(value) != grayFrequency.end())
             {
-                for (int x = 0; x < cropped.width; x++)
-                {
-                    Color currentColor = GetImageColor(cropped, x, y);
-                    
-                    r += currentColor.r;
-                    g += currentColor.g;
-                    b += currentColor.b;
-                    a += currentColor.a;
-                }
+                textures[grayFrequency[value]]->frequency += 1;
             }
-            r /= (cropped.width) * (cropped.height);
-            g /= (cropped.width) * (cropped.height);
-            b /= (cropped.width) * (cropped.height);
-            a /= (cropped.width) * (cropped.height);
-            avgColors[(i * COLS) + j] = ColorComparer(r,g,b,a);
-            textures[(i * COLS) + j] = texture;
+            else if (value < 0.95)
+            {
+                grayFrequency.emplace(value, (i * COLS) + j);
+            }
         }
     }
-    // Make a histogram
-    // Convert image to black and white by averaging the RGB values
-    // determine size of image
-    // determine max intensity
-    // set the values for the histogram
-    // compute the frequency (between 0 and max)
-    // 
 
-    //std::sort(std::begin(avgColors), std::end(avgColors));
-    /*std::map<int, ColorComparer> colorMap;
-
-    int index = 0;
-    
-    while (index < std::size(avgColors))
+    // Used to hold frequency and texture arr locations for future sorting
+    std::vector<std::pair<int, int>> holderVec(grayFrequency.size()); // first is frequency, second is loc in text arr
+    for (auto i : grayFrequency)
     {
-        ColorComparer current = avgColors[index];
+        holderVec.push_back(std::make_pair(textures[i.second]->frequency, i.second));
+    }
 
-    }*/
+    int index = 0;    
+    std::vector<std::pair<int, int>> sortedFrequency; // first is frequency, second is loc in text arr
+    sortedFrequency.push_back(std::make_pair(holderVec[0].first, holderVec[0].second));
+    // Eliminates values that are similar to other values
+    for (int i = 1; i < holderVec.size(); i++)
+    {
+        float currentVal = textures[holderVec[i].second]->hsv.z;
+        float comparedVal = textures[holderVec[index].second]->hsv.z;
+        if (!(comparedVal + 0.2 > currentVal && comparedVal - 0.2 < currentVal))
+        {
+            sortedFrequency.push_back(std::make_pair(holderVec[i].first, holderVec[i].second));
+            index = i;
+        }
+    }
 
-    std::set<ColorComparer> imageColors{ std::begin(avgColors), std::end(avgColors) };
-    std::cout << imageColors.size() << std::endl;
+    // Sort so that those with higher frequency are at the beginning
+    std::sort(sortedFrequency.begin(), sortedFrequency.end(), std::greater<std::pair<int, int>>());
+    if (sortedFrequency.size() > COLOR_AMOUNT)
+        sortedFrequency.resize(COLOR_AMOUNT);
+
     GuiLoadStyle(ASSETS_PATH"style_bluish.rgs");
     
     while (!WindowShouldClose())
@@ -159,26 +142,43 @@ int main(void)
         {
             for (int j = 0; j < COLS; j++)
             {
+                int loc = (i * COLS) + j;
+                TextureTile textureTile = *textures[loc];
+                Color color = textureTile.imageColor;
 
-                Texture2D texture = textures[(i * COLS) + j];
+                if (textureTile.hsv.z < 0.95) // ignores white values
+                {
+                    // Find color that most closely matches one of the high frequency values
+                    float smallestDist = 1;
+                    for (auto i : sortedFrequency)
+                    {
+                        float currentDist = abs(textures[i.second]->hsv.z - textureTile.hsv.z);
+                        if (currentDist < smallestDist)
+                        {
+                            smallestDist = currentDist;
+                            color = textures[i.second]->imageColor;
+                        }
+                    }
+                }
+
+                // Draw colored rectangle
+                Texture2D texture = textureTile.texture;
                 const int rectX = (j * texture.width * PADDING) + (SCREEN_WIDTH / 2 - image.width / 2);
                 const int rectY = (i * texture.height * PADDING) + (SCREEN_HEIGHT / 2 - image.height / 2);
-                DrawRectangle(rectX, rectY, texture.width, texture.height, avgColors[(i * COLS) + j].color);
+                DrawRectangle(rectX, rectY, texture.width, texture.height, color);
             }
         }
 
+        // Display colors being used in limited color picture
         int index = 0;
         int width = 40;
         int height = 40;
-        for (ColorComparer const& color : imageColors)
+        for (auto i : sortedFrequency)
         {
-            if (index % (imageColors.size() / COLOR_AMOUNT) == 0)
-            {
-                const int rectX = (index * width);
-                const int rectY = (SCREEN_HEIGHT - height);
-                DrawRectangle(rectX, rectY, width, height, color.color);
-            }
-            
+            const int rectX = (index * width);
+            const int rectY = (SCREEN_HEIGHT - height);
+            DrawRectangle(rectX, rectY, width, height, textures[i.second]->imageColor);
+
             index++;
         }
 
